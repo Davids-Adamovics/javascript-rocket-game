f24af43ee0e823ecb700754be4432435eff56e3f
Davids-Adamovics <davidsadamovicss@gmail.com>
2024-04-30 13:23:30 +0300

    added meteorites

--- myscripts.js
+++ myscripts.js
@@ -1,23 +1,31 @@
+// ============== CANVAS ==============
 let canvasWidth = 800;
 let canvasHeight = 600;
+var boostLabel;
+var interval = setInterval(updateCanvas, 20);
 
+// ============== Player ==============
 var player;
-var playerXPosition = canvasWidth / 2 - 30;
-var playerYPosition = canvasHeight / 2 - 30;
+var playerXPosition = canvasWidth / 2 - 30; //player spawn location
+var playerYPosition = canvasHeight / 2 - 30; //player spawn location
 var spaceShip = new Image();
 spaceShip.src = 'img/img.png';
 var rotation = 0; // Assuming this should not be 'this.rotation'
 var boost = false;
 var speed = 4;
 let boostAmount = 100; // Correctly initialized and globally accessible
-var boostLabel;
 
+// ============== Meteorite ==============
+var meteorImg = new Image();
+meteorImg.src = 'img/metor.png';
+var meteorite;
 var gravity = 0;
-var interval = setInterval(updateCanvas, 20);
+var meteoriteXposition = 0;
+var meteoriteYposition = 0;
+var alive = true;
 
-// Key tracking
-var keys = {};
-
+// ============== Keys ==============
+var keys = {}; // Key tracking
 // key pressed
 document.addEventListener('keydown', function (event) {
     keys[event.keyCode] = true;
@@ -27,7 +35,6 @@ document.addEventListener('keydown', function (event) {
         }
     }
 });
-
 // Key released
 document.addEventListener('keyup', function (event) {
     delete keys[event.keyCode];
@@ -38,11 +45,24 @@ document.addEventListener('keyup', function (event) {
 
 
 // start game function
+var allmeteorites = []; // Move this to a more global scope if necessary
+
 function startGame() {
     gameCanvas.start();
-    player = new createPlayer(50, 50);  // creates a new player
-    boostLabel = new createBoostLabel(10, 30);  // creates a new label for displaying boost
+    player = new createPlayer(50, 50);  // Creates a new player
+
+    for (var i = 1; i < 10; i++) {
+        setTimeout(function() {
+            var meteorite = new createMetorite(50, 50); // Creates a new meteorite
+            allmeteorites.push(meteorite); // Add the new meteorite to the array
+        }, i * 1000);
+    }
+
+    boostLabel = new createBoostLabel(10, 30);  // Creates a new label for displaying boost
 }
+
+
+
 
 // defines the game field with canvas
 // store the data of a canvas
@@ -86,7 +106,7 @@ function createPlayer(width, height) {
     //     this.stopPlayer();
     // }
 
-    this.stopPlayer = function () {
+    this.stopPlayer = function () { // borders
         var ground = canvasHeight - this.height;
         var top = 0;
         var leftSide = 0;
@@ -112,9 +132,9 @@ function createPlayer(width, height) {
 
     this.movePlayer = function () {
         var speed = boost ? 8 : 4; // Use ternary operator for clarity
-        boost ? boostAmount-- : boostAmount+= 0.5;
-        if(boostAmount>100) boostAmount = 100;
-        if(boostAmount<0) boostAmount = 0;
+        boost ? boostAmount-- : boostAmount += 0.5;
+        if (boostAmount > 100) boostAmount = 100;
+        if (boostAmount < 0) boostAmount = 0;
 
         if (keys[37]) { // Left arrow
             this.x -= speed;
@@ -150,22 +170,123 @@ function createPlayer(width, height) {
 
 }
 
-function updateCanvas() {
-    ctx = gameCanvas.context;
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+function createMetorite(width, height) {
+    this.width = width;
+    this.height = height;
+    var xyCoordinatesStart = randomMeteoriteStartLocation();
+    this.xstart = xyCoordinatesStart["x"];
+    this.ystart = xyCoordinatesStart["y"];
+    var xyCoordinatesEnd = randomMeteoriteEndLocation();
+    this.xend = xyCoordinatesEnd["x"];
+    this.yend = xyCoordinatesEnd["y"];
 
-    // player.makeFall();
+
+    this.draw = function () {
+        const ctx = gameCanvas.context;
+        ctx.save(); // Save the current context state
+        ctx.translate(this.xstart + this.width / 2, this.ystart + this.height / 2); // Move context to center of the image
+        ctx.rotate(this.rotation); // Rotate context
+        if (spaceShip.complete) {
+            ctx.drawImage(meteorImg, -this.width / 2, -this.height / 2, this.width, this.height);
+        } else {
+            spaceShip.onload = () => {
+                ctx.drawImage(meteorImg, -this.width / 2, -this.height / 2, this.width, this.height);
+            };
+        }
+        ctx.restore(); // Restore context to its original state
+    };
+
+    this.makeFall = function () {
+        // Calculate distance between current position and end position
+        var deltaX = this.xend - this.xstart;
+        var deltaY = this.yend - this.ystart;
+
+        // Determine step size based on gravity or a constant speed
+        var stepSize =4 ;  // Or some other appropriate value
+
+        // Normalize the distance vector
+        var distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        var normX = deltaX / distance;
+        var normY = deltaY / distance;
+
+        // Move meteorite by normalized vector scaled by the step size
+        if (distance > stepSize) { // Check if the meteorite has not yet reached the endpoint
+            this.xstart += normX * stepSize;
+            this.ystart += normY * stepSize;
+        } else {
+            this.xstart = this.xend; // Snap to end position if very close
+            this.ystart = this.yend;
+        }
+    };
+}
+function randomMeteoriteStartLocation() {
+    var dict = new Object();
+    let x = Math.floor(Math.random() * 2); // Nāks no X vai Y {0 == X} {1 == Y}
+
+    if (x == 0) {
+        let xWidthRandomLocation = Math.floor(Math.random() * canvasWidth) + 1;
+        console.log("x start random location", xWidthRandomLocation);
+        var dict = {
+            x: xWidthRandomLocation,
+            y: 0
+        };
+        return dict;
+    } else {
+        let yWidthRandomLocation = Math.floor(Math.random() * canvasHeight) + 1;
+        console.log("y start random location", yWidthRandomLocation);
+        var dict = {
+            x: 0,
+            y: yWidthRandomLocation
+        };
+        return dict;
+    }
+}
+
+function randomMeteoriteEndLocation() {
+    var dict = new Object();
+    let x = Math.floor(Math.random() * 2); // Nāks no X vai Y {0 == X} {1 == Y}
+
+    if (x == 0) {
+        let xWidthRandomLocation = Math.floor(Math.random() * canvasWidth) + 1;
+        console.log("x end random location", xWidthRandomLocation);
+        var dict = {
+            x: xWidthRandomLocation,
+            y: canvasHeight
+        };
+        return dict;
+    } else {
+        let yWidthRandomLocation = Math.floor(Math.random() * canvasHeight) + 1;
+        console.log("y end random location", yWidthRandomLocation);
+        var dict = {
+            x: canvasWidth,
+            y: yWidthRandomLocation
+        };
+        return dict;
+    }
+}
+
+function updateCanvas() {
+    var ctx = gameCanvas.context;
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    // Update and draw each meteorite
+    allmeteorites.forEach(function (meteorite) {
+        meteorite.makeFall();
+        meteorite.draw();
+    });
+
+    // Move and draw player
     player.movePlayer();
     manageBoost();
     player.draw();
     boostLabel.draw();
-
 }
+
 
 function createBoostLabel(x, y) {
     this.x = x;
     this.y = y;
-    this.draw = function() {
+    this.draw = function () {
         ctx = gameCanvas.context;
         ctx.font = "25px Marker Felt";  // Setting font for the text
         ctx.fillStyle = "black";  // Setting text color
